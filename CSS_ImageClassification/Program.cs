d5c0b9169d3114163ccac52ab7534974002b8219
Lê Hoàng Thiên Vũ <thienvu.lh@capnuocnhabe.onmicrosoft.com>
2022-09-19 16:27:24 +0700

    Update

--- CSS_ImageClassification/Program.cs
+++ CSS_ImageClassification/Program.cs
@@ -1,8 +1,8 @@
 ﻿////Load sample data
 //using CSS_ImageClassification;
 //using Microsoft.ML;
-//using OpenCvSharp;
-//using OpenCvSharp.Extensions;
+using OpenCvSharp;
+using OpenCvSharp.Extensions;
 //using Tensorflow.Keras.Engine;
 //using Tensorflow.Keras.Layers;
 //using static Tensorflow.KerasApi;
@@ -13,14 +13,17 @@ using Keras.Layers;
 using Keras.Optimizers;
 using Keras.Utils;
 using Numpy;
-using System;
-using System.IO;
-using System.Text;
+using Tensorflow;
+//using Tensorflow.Layers;
+//using System;
+//using System.IO;
+//using System.Text;
 using Keras.Datasets;
-
+using System;
+using Python.Runtime;
 
 ////Load model and predict output
-//Size size = new Size(28, 28);
+
 ////string folderPath = @"F:\Projects\PythonFile\LTQG\DuLieuBaoCao\TestClassifier\Clean\";
 //string folderPath = @"D:\0.Projects\Z_PyThon\LTQG\DuLieuBaoCao\TestClassifier\Clean\";
 //foreach (string folder in Directory.EnumerateFiles(folderPath, "*"))
@@ -107,66 +110,91 @@ using Keras.Datasets;
 int batch_size = 128;
 int num_classes = 10;
 int epochs = 1;
-
-// input image dimensions
-int img_rows = 28, img_cols = 28;
-
+int img_rows = 28;
+int img_cols = 28;
 Shape input_shape = null;
+Size size = new Size(28, 28);
+string folderPath = @"D:\0.Projects\CSS_ImageClassification-master\CSS_ImageClassification\Test\";
 
 // the data, split between train and test sets
 var ((x_train, y_train), (x_test, y_test)) = MNIST.LoadData();
+Console.WriteLine(x_train[0]);
+Console.WriteLine($"x_train shape: {x_train[0].shape}");
+Console.WriteLine($"x_train type: {x_train[0].GetType()}");
 
-if (Backend.ImageDataFormat() == "channels_first")
-{
-    x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols);
-    x_test = x_test.reshape(x_test.shape[0], 1, img_rows, img_cols);
-    input_shape = (1, img_rows, img_cols);
-}
-else
-{
-    x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1);
-    x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1);
-    input_shape = (img_rows, img_cols, 1);
-}
+Mat img = Cv2.ImRead(folderPath + "temp2.jpg", ImreadModes.Color);
+Mat imgGray = new Mat();
+Cv2.CvtColor(img, imgGray, ColorConversionCodes.BGR2GRAY);
+Mat imgThres = new Mat();
+Cv2.Threshold(imgGray, imgThres, 150, 250, ThresholdTypes.Otsu);
+Mat imgResize = new Mat();
+Cv2.Resize(imgThres, imgResize, size);
+Console.WriteLine("==========================");
+Console.WriteLine(imgResize);
+Console.WriteLine($"imgResize size : {imgResize.Size()}");
+Console.WriteLine($"imgResize type : {imgResize.GetType()}");
 
-x_train = x_train.astype(np.float32);
-x_test = x_test.astype(np.float32);
-x_train /= 255;
-x_test /= 255;
-Console.WriteLine($"x_train shape: {x_train.shape}");
-Console.WriteLine($"{x_train.shape[0]} train samples");
-Console.WriteLine($"{x_test.shape[0]} test samples");
+//if (Backend.ImageDataFormat() == "channels_first")
+//{
+//    x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols);
+//    x_test = x_test.reshape(x_test.shape[0], 1, img_rows, img_cols);
+//    input_shape = (1, img_rows, img_cols);
+//}
+//else
+//{
+//    x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1);
+//    x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1);
+//    input_shape = (img_rows, img_cols, 1);
+//}
 
-// convert class vectors to binary class matrices
-y_train = Util.ToCategorical(y_train, num_classes);
-y_test = Util.ToCategorical(y_test, num_classes);
+//x_train = x_train.astype(np.float32);
+//x_test = x_test.astype(np.float32);
+//x_train /= 255;
+//x_test /= 255;
+//Console.WriteLine($"x_train shape: {x_train.shape}");
+//Console.WriteLine($"{x_train.shape[0]} train samples");
+//Console.WriteLine($"{x_test.shape[0]} test samples");
 
-// Build CNN model
-var model = new Sequential();
-model.Add(new Input(shape : input_shape));
-model.Add(new Conv2D(32, kernel_size: (3, 3).ToTuple(),
-                        activation: "relu",
-                        input_shape: input_shape));
-model.Add(new Conv2D(64, (3, 3).ToTuple(), activation: "relu"));
-model.Add(new MaxPooling2D(pool_size: (2, 2).ToTuple()));
-model.Add(new Dropout(0.25));
-model.Add(new Flatten());
-model.Add(new Dense(128, activation: "relu"));
-model.Add(new Dropout(0.5));
-model.Add(new Dense(num_classes, activation: "softmax"));
+//// convert class vectors to binary class matrices
+//y_train = Util.ToCategorical(y_train, num_classes);
+//y_test = Util.ToCategorical(y_test, num_classes);
 
-model.Compile(loss: "categorical_crossentropy",
-    optimizer: new Adadelta(), metrics: new string[] { "accuracy" });
+//// Build CNN model
+//var model = new Sequential();
 
-model.Fit(x_train, y_train,
-            batch_size: batch_size,
-            epochs: epochs,
-            verbose: 1,
-            validation_data: new NDarray[] { x_test, y_test });
-var score = model.Evaluate(x_test, y_test, verbose: 0);
-Console.WriteLine($"Test loss: {score[0]}");
-Console.WriteLine($"Test accuracy: {score[1]}");
+//model.Add(new Conv2D(32, kernel_size: (3, 3).ToTuple(),
+//                        activation: "relu",
+//                        input_shape: input_shape));
+//model.Add(new Conv2D(64, (3, 3).ToTuple(), activation: "relu"));
+//model.Add(new MaxPooling2D(pool_size: (2, 2).ToTuple()));
+//model.Add(new Dropout(0.25));
+//model.Add(new Flatten());
+//model.Add(new Dense(128, activation: "relu"));
+//model.Add(new Dropout(0.5));
+//model.Add(new Dense(num_classes, activation: "softmax"));
 
+//model.Compile(loss: "categorical_crossentropy",
+//    optimizer: new Adadelta(), metrics: new string[] { "accuracy" });
+
+//model.Summary();
+
+//model.Fit(x_train, y_train,
+//            batch_size: batch_size,
+//            epochs: 1,
+//            verbose: 1,
+//            validation_data: new NDarray[] { x_test, y_test });
+
+//var score = model.Evaluate(x_test, y_test, verbose: 0);
+//Console.WriteLine($"Test loss: {score[0]}");
+//Console.WriteLine($"Test accuracy: {score[1]}");
+
+//Mat img = Cv2.ImRead(folderPath);
+//Mat imgGray = new Mat();
+//Cv2.CvtColor(img, imgGray, ColorConversionCodes.BGR2GRAY);
+//Mat imgThres = new Mat();
+//Cv2.Threshold(imgGray, imgThres, 150, 250, ThresholdTypes.Otsu);
+//Mat imgResize = new Mat();
+//Cv2.Resize(imgThres, imgResize, size);
 
 //Load sample data
 //using CSS_ImageClassification;
