f05b4b8c82b3323226e3b99570f4a05359e08c49
Lê Hoàng Thiên Vũ <thienvu.lh@capnuocnhabe.onmicrosoft.com>
2022-09-20 16:02:23 +0700

    Update build model

--- CSS_ImageClassification/Program.cs
+++ CSS_ImageClassification/Program.cs
@@ -3,10 +3,6 @@
 //using Microsoft.ML;
 using OpenCvSharp;
 using OpenCvSharp.Extensions;
-//using Tensorflow.Keras.Engine;
-//using Tensorflow.Keras.Layers;
-//using static Tensorflow.KerasApi;
-//using static Tensorflow.Binding;
 using Keras;
 using Keras.Models;
 using Keras.Layers;
@@ -14,14 +10,9 @@ using Keras.Optimizers;
 using Keras.Utils;
 using Numpy;
 using Tensorflow;
-//using Tensorflow.Layers;
-//using System;
-//using System.IO;
-//using System.Text;
 using Keras.Datasets;
 using System;
 using Python.Runtime;
-using Tensorflow.Keras.Layers;
 using System.IO;
 
 ////Load model and predict output
@@ -110,23 +101,17 @@ using System.IO;
 //          validation_split: 0.2f);
 
 int batch_size = 128;
-int num_classes = 40;
-int epochs = 1;
+int num_classes = 41;
+int epochs = 50;
 int img_size = 28;
-Shape input_shape = (1, img_size, img_size, 1);
+Shape input_shape = (img_size, img_size, 1);
 Size size = new Size(img_size, img_size);
-string folderTrainPath = @"F:\Projects\CSS_ImageClassification_02\CSS_ImageClassification\Train\";
-string folderTestPath = @"F:\Projects\CSS_ImageClassification_02\CSS_ImageClassification\Test\";
-
+string folderTrainPath = @"D:\0.Projects\CSS_ImageClassification-master\CSS_ImageClassification\Train\"; 
+string folderTestPath = @"D:\0.Projects\CSS_ImageClassification-master\CSS_ImageClassification\Test\";
+string modelH5Path = @"D:\0.Projects\CSS_ImageClassification-master\CSS_ImageClassification\Model\myClassificationModel.h5";
+string modelOnnxPath = @"D:\0.Projects\CSS_ImageClassification-master\CSS_ImageClassification\Model\myClassificationModel.onnx";
 // the data, split between train and test sets
-var ((x_train, y_train), (x_test, y_test)) = MNIST.LoadData();
-//Console.WriteLine($"{x_train[0]}");
-//Console.WriteLine($"{y_train[0]}");
-//Console.WriteLine($"{y_train[1]}");
-//Console.WriteLine($"{x_test[0]}");
-//Console.WriteLine($"{y_test[0]}");
-//x_train = x_train.reshape(-1, img_size, img_size, 1);
-//x_test = x_test.reshape(-1, img_size, img_size, 1);
+//var ((x_train, y_train), (x_test, y_test)) = MNIST.LoadData();
 //if (Backend.ImageDataFormat() == "channels_first")
 //{
 //    x_train = x_train.reshape(x_train.shape[0], 1, img_size, img_size);
@@ -147,27 +132,27 @@ var ((x_train, y_train), (x_test, y_test)) = MNIST.LoadData();
 //x_train /= 255;
 //x_test /= 255;
 
-//Console.WriteLine($"{x_train.shape[0]} train samples");
-//Console.WriteLine($"{x_test.shape[0]} test samples");
+////Console.WriteLine($"{x_train.shape[0]} train samples");
+////Console.WriteLine($"{x_test.shape[0]} test samples");
 
 ////// convert class vectors to binary class matrices
 //y_train = Util.ToCategorical(y_train, 10);
-//y_test = Util.ToCategorical(y_test, num_classes);
+//y_test = Util.ToCategorical(y_test, 10);
 
-Console.WriteLine($"{x_train[0].shape}");
-Console.WriteLine($"{y_train}");
+//Console.WriteLine($"{x_train}");
+//Console.WriteLine($"{x_train[0].shape}");
+//Console.WriteLine($"{y_train}");
+//Console.WriteLine($"{y_train[0]}");
 
 
-List<NDarray> train_x = new List<NDarray>();
-List<NDarray> train_y = new List<NDarray>();
+List<NDarray> train_x_pre = new List<NDarray>();
+List<NDarray> train_y_pre = new List<NDarray>();
 Console.WriteLine("========================== Load Training Data =================================");
 foreach (string folder in Directory.EnumerateDirectories(folderTrainPath, "*"))
 {
     Console.WriteLine($">>>>>>>>>>>>>>>>>>>>> Load Folder : {Path.GetFileName(folder)}");
     int label = int.Parse(Path.GetFileName(folder));
-    //NDarray label_y = Util.ToCategorical((NDarray)label, num_classes);
     NDarray label_y = (NDarray)label;
-    Console.WriteLine(label_y);
     foreach (string file in Directory.EnumerateFiles(folder, "*.jpg"))
     {
         Mat img = Cv2.ImRead(file, ImreadModes.Color);
@@ -178,78 +163,83 @@ foreach (string folder in Directory.EnumerateDirectories(folderTrainPath, "*"))
         Mat imgResize = new Mat();
         Cv2.Resize(imgThres, imgResize, size , interpolation : InterpolationFlags.Area);
         imgResize.GetArray(out byte[] plainArray);
-        NDarray imgNDArray = np.array(plainArray, dtype: np.uint8);
-        NDarray imgNDArray_reshape = imgNDArray.reshape(-1, img_size, img_size, 1);
-        NDarray imgNDArray_train = imgNDArray_reshape.astype(np.float32);
-        train_x.add(imgNDArray_train);
-        train_y.add(label_y);
-        //Console.WriteLine(imgNDArray);
-        //Console.WriteLine($"imgNDArray shape : {imgNDArray.shape}");
-        //NDarray imgNDArray_train = imgNDArray.reshape(-1, img_size, img_size, 1);
-        //Console.WriteLine("==========================");
-        //Console.WriteLine($"imgNDArray_train shape: {imgNDArray_train.shape}");
-        break;
+        NDarray imgNDArray = np.array(plainArray, dtype: np.uint8).reshape(img_size,img_size);
+        train_x_pre.add(imgNDArray);
+        train_y_pre.add(label_y);
     }
-    break;
 }
 
-//Console.WriteLine($"{train_x[0]}");
-Console.WriteLine($"{train_x[0].shape}");
-Console.WriteLine($"{train_y[0]}");
+NDarray train_x = np.array(train_x_pre);
+NDarray train_y = np.array(train_y_pre);
+train_x = train_x.reshape(train_x.shape[0], img_size, img_size, 1);
+train_x = train_x.astype(np.float32);
+train_y = Util.ToCategorical(train_y, num_classes);
 
-//NDarray test_x;
-//NDarray test_y;
-//foreach (string folder in Directory.EnumerateDirectories(Path.Combine(folderTestPath, "Clean_02"), "*"))
-//{
-//    Console.WriteLine("==========================");
-//    Console.WriteLine($"{folder}");
-//    var label = Path.GetFileName(folder);
-//    Console.WriteLine($"{label}");
-//    foreach (string file in Directory.EnumerateFiles(folder, "*.jpg"))
-//    {
-//        Console.WriteLine($"{file}");
-//        break;
-//    }
-//    break;
-//}
+List<NDarray> test_x_pre = new List<NDarray>();
+List<NDarray> test_y_pre = new List<NDarray>();
+Console.WriteLine("========================== Load Testing Data =================================");
+foreach (string folder in Directory.EnumerateDirectories(Path.Combine(folderTestPath, "Clean_02"), "*"))
+{
+    Console.WriteLine($">>>>>>>>>>>>>>>>>>>>> Load Folder : {Path.GetFileName(folder)}");
+    int label = int.Parse(Path.GetFileName(folder));
+    NDarray label_y = (NDarray)label;
+    foreach (string file in Directory.EnumerateFiles(folder, "*.jpg"))
+    {
+        Mat img = Cv2.ImRead(file, ImreadModes.Color);
+        Mat imgGray = new Mat();
+        Cv2.CvtColor(img, imgGray, ColorConversionCodes.BGR2GRAY);
+        Mat imgThres = new Mat();
+        Cv2.Threshold(imgGray, imgThres, 150, 250, ThresholdTypes.Otsu);
+        Mat imgResize = new Mat();
+        Cv2.Resize(imgThres, imgResize, size, interpolation: InterpolationFlags.Area);
+        imgResize.GetArray(out byte[] plainArray);
+        NDarray imgNDArray = np.array(plainArray, dtype: np.uint8).reshape(img_size, img_size);
+        test_x_pre.add(imgNDArray);
+        test_y_pre.add(label_y);
+    }
+}
 
+NDarray test_x = np.array(test_x_pre);
+NDarray test_y = np.array(test_y_pre);
+test_x = test_x.reshape(test_x.shape[0], img_size, img_size, 1);
+test_x = test_x.astype(np.float32);
+test_y = Util.ToCategorical(test_y, num_classes);
 
-//// Build CNN model
-//var model = new Sequential();
+Console.WriteLine($"{train_x.shape[0]} train samples");
+Console.WriteLine($"{test_x.shape[0]} test samples");
 
-//model.Add(new Conv2D(32, kernel_size: (3, 3).ToTuple(),
-//                        activation: "relu",
-//                        input_shape: input_shape));
-//model.Add(new Conv2D(64, (3, 3).ToTuple(), activation: "relu"));
-//model.Add(new MaxPooling2D(pool_size: (2, 2).ToTuple()));
-//model.Add(new Dropout(0.25));
-//model.Add(new Flatten());
-//model.Add(new Dense(128, activation: "relu"));
-//model.Add(new Dropout(0.5));
-//model.Add(new Dense(num_classes, activation: "softmax"));
+// Build CNN model
+var model = new Sequential();
 
-//model.Compile(loss: "categorical_crossentropy",
-//    optimizer: new Adadelta(), metrics: new string[] { "accuracy" });
+model.Add(new Conv2D(32, kernel_size: (3, 3).ToTuple(),
+                        activation: "relu",
+                        input_shape: input_shape));
+model.Add(new Conv2D(64, (3, 3).ToTuple(), activation: "relu"));
+model.Add(new MaxPooling2D(pool_size: (2, 2).ToTuple()));
+model.Add(new Dropout(0.25));
+model.Add(new Flatten());
+model.Add(new Dense(128, activation: "relu"));
+model.Add(new Dropout(0.5));
+model.Add(new Dense(num_classes, activation: "softmax"));
 
-//model.Summary();
+model.Compile(loss: "categorical_crossentropy",
+    optimizer: new Adadelta(), metrics: new string[] { "accuracy" });
 
-//model.Fit(x_train, y_train,
-//            batch_size: batch_size,
-//            epochs: 1,
-//            verbose: 1,
-//            validation_data: new NDarray[] { x_test, y_test });
+model.Summary();
 
-//var score = model.Evaluate(x_test, y_test, verbose: 0);
-//Console.WriteLine($"Test loss: {score[0]}");
-//Console.WriteLine($"Test accuracy: {score[1]}");
+model.Fit(train_x, train_y,
+            batch_size: batch_size,
+            epochs: epochs,
+            verbose: 1
+            //validation_data: new NDarray[] { test_x, test_y }
+            );
 
-//Mat img = Cv2.ImRead(folderPath);
-//Mat imgGray = new Mat();
-//Cv2.CvtColor(img, imgGray, ColorConversionCodes.BGR2GRAY);
-//Mat imgThres = new Mat();
-//Cv2.Threshold(imgGray, imgThres, 150, 250, ThresholdTypes.Otsu);
-//Mat imgResize = new Mat();
-//Cv2.Resize(imgThres, imgResize, size);
+var score = model.Evaluate(test_x, test_y, verbose: 0);
+Console.WriteLine($"Test loss: {score[0]}");
+Console.WriteLine($"Test accuracy: {score[1]}");
+
+model.Save(modelH5Path);
+model.SaveOnnx(modelOnnxPath);
 
 //Load sample data
 //using CSS_ImageClassification;
